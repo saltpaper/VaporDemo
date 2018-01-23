277666f67df29a0b45ef4166192af18bc3f8576d
Amy Dong <jing.dong@mewme.com>
2018-01-23 18:20:46 +0800

    Add home and userlist page

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,7 +1,18 @@
 import Vapor
 
 extension Droplet {
-    func setupRoutes() throws {
+    func setupRoutes() throws {   //This method is where all the routes for our application will be added.
+        get("home") { req in
+            return try self.view.make("homeView.leaf", ["name": "amy"])
+        }
+        
+        get("name",":name") { request in
+            if let name = request.parameters["name"]?.string {
+                return "Hello \(name)!"
+            }
+            return "Error retrieving parameters."
+        } // website: .../name/amy
+        
         get("hello") { req in
             var json = JSON()
             try json.set("hello", "world")
@@ -20,6 +31,11 @@ extension Droplet {
 
         get("description") { req in return req.description }
         
+        get("usersList") {req in
+            return try self.view.make("userList.leaf")
+        }
+        
         try resource("posts", PostController.self)
     }
+    
 }
